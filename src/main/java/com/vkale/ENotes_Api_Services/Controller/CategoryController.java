c8b1c968c214ad89d63011690766c99d88e5bb45
Viraj07 <virajkale20@gmail.com>
2024-12-16 12:14:33 +0530

    Exception added
    Implemented Exist Category Exception

--- src/main/java/com/vkale/ENotes_Api_Services/Controller/CategoryController.java
+++ src/main/java/com/vkale/ENotes_Api_Services/Controller/CategoryController.java
@@ -13,6 +13,7 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/category")
--- src/main/java/com/vkale/ENotes_Api_Services/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/vkale/ENotes_Api_Services/Exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.vkale.ENotes_Api_Services.Exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,25 +14,35 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception e) {
-       // log.error("GlobalExceptionHandler :: handleException ::", e.getMessage());
+        log.error("GlobalExceptionHandler :: handleException ::", e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<?> handleNullPointerException(Exception e) {
-        //log.error("GlobalExceptionHandler :: handleNullPointerException ::", e.getMessage());
+        log.error("GlobalExceptionHandler :: handleNullPointerException ::", e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(Exception e) {
-       // log.error("GlobalExceptionHandler :: handleResourceNotFoundException ::", e.getMessage());
+        log.error("GlobalExceptionHandler :: handleResourceNotFoundException ::", e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<?> handleValidationException(ValidationException e) {
         return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ExistDataException.class)
+    public ResponseEntity<?> handleExistDataException(ExistDataException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
